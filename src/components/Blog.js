7bc838b4f17524ff7c5e1899f589015b8ea5be6f
Danielle Hoopes <danielle.hoopes@snhu.edu>
2023-02-13 16:08:24 -0700

    Update Blog.js

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Blog = () => {
   return (
-    <section className="bg-primary text-white px-5 py-32" id="blog">
-      <div className="container mx-auto grid md:grid-cols-2 items-center justify-center md:justify-between">
+    <section className="bg-primary text-white " id="blog">
+      <div className="container mx-auto  items-center justify-center md:justify-between">
         <div className="about-info">
         <iframe width="360" height="440"  src="https://rss.app/embed/v1/carousel/2ytTHH88xrAVfOeI" frameborder="0"></iframe>
         </div>
