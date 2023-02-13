b267c23fc0e6fea3a39b8d8994fe3a9550e6ba12
Danielle Hoopes <daniellehoopes1@gmail.com>
2023-02-12 19:27:40 -0700

    Form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,28 +7,56 @@ import {
 
 const Contact = () => {
   return (
-    <section className="bg-secondary text-white px-5 py-32" id="contact">
-      <div className="container mx-auto grid md:grid-cols-2 items-center justify-center md:justify-between">
-        <div className="about-info">
-       
-        <h2 className="text-4xl  text-center font-bold mb-5 border-b-[5px] w-[180px] border-blue pb-2">
-            Contact Me
-          </h2>
-          <p className="pb-5">
-          Visit one of my social links or: 
-          </p>
-            
-          <a href="mailto:hereshecodes1@gmail.com"
-          className="pr-4  text-accent hover:text-white">
-
-            hereshecodes1@gmail.com
-            {" "}
-              <AiOutlineMail size={40} />{" "}
+<section id="contact" className="bg-secondary">
+      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+          <iframe
+            width="100%"
+            height="100%"
+            title="map"
+            className="absolute inset-0"
+            frameBorder={0}
+            marginHeight={0}
+            marginWidth={0}
+            style={{ filter: "opacity(0.7)" }}
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d773465.8297770084!2d-112.47952465529956!3d40.77629923160011!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87523d9488d131ed%3A0x5b53b7a0484d31ca!2sSalt%20Lake%20City%2C%20UT!5e0!3m2!1sen!2sus!4v1676249437097!5m2!1sen!2sus" 
+          />
+          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+            <div className="lg:w-1/2 px-6">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                ADDRESS
+              </h2>
+              <p className="mt-1">
+                Salt Lake City, Utah
+              </p>
+            </div>
+            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                EMAIL
+              </h2>
+              <a className="text-indigo-400 leading-relaxed">
+                hereshecodes1@gmail.com
               </a>
-
+            
+            </div>
+          </div>
         </div>
-
- 
+       <form action="/action_page.php"
+          netlify
+          name="contact"
+          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+            Hire Me
+          </h2>
+          <p className="leading-relaxed mb-5">
+           I am always happy to hear about projects your working on
+           and how I can help. 
+          </p>
+          
+         
+          
+         
+        </form>
       </div>
     </section>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,6 +16,7 @@ const Projects = () => {
         to see what I'm working on
           </p>
 
+          <p>In backend I know Node.js, C#, SQL Server and MongoDB</p>
 
         </div>
 
