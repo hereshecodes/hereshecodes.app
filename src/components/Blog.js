a968c9799aed2a4bf96f89d157336a9b5f915f1e
Danielle Hoopes <danielle.hoopes@snhu.edu>
2023-02-13 16:22:17 -0700

    Update Blog.js

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -4,7 +4,7 @@ const Blog = () => {
   return (
     <section className="bg-primary text-white px-5 py-32" id="blog">
       <div className="container mx-auto grid md:grid-cols-2 items-center justify-center md:justify-between">
-        <div className="scrollbar-hide overflow-auto">
+        <div className="stack-carousel scrollbar-hide overflow-auto">
         <iframe width="1000" height="440"  src="https://rss.app/embed/v1/carousel/2ytTHH88xrAVfOeI" frameborder="0"></iframe>
         </div>
       </div>
