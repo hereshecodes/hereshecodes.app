52d5fc25b7278ccf6409166312517cf42e64b4f4
Danielle Hoopes <danielle.hoopes@snhu.edu>
2023-02-13 16:02:59 -0700

    Update Blog.js

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -6,8 +6,7 @@ const Blog = () => {
     <section className="bg-secondery text-white px-5 py-32" id="blog">
       <div className="container mx-auto grid md:grid-cols-2 items-center justify-center md:justify-between">
         <div className="about-info">
-       
-          <rssapp-carousel id="2ytTHH88xrAVfOeI"></rssapp-carousel><script src="https://widget.rss.app/v1/carousel.js" type="text/javascript" async></script>
+        <iframe width="360" height="440"  src="https://rss.app/embed/v1/carousel/2ytTHH88xrAVfOeI" frameborder="0"></iframe>
 
         </div>
 
