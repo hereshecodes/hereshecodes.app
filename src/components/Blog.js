fd891df56516c714124ec721ba5bd82fb6f9da67
Danielle Hoopes <danielle.hoopes@snhu.edu>
2023-02-13 16:37:20 -0700

    Update Blog.js

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -5,7 +5,7 @@ const Blog = () => {
     <section className="bg-primary text-white px-5 py-32" id="blog">
       <div className="container mx-auto  items-center justify-center md:justify-between">
         <div className="stack-carousel scrollbar-hide overflow-auto">
-        <iframe max-width="100%" height="440"  src="https://rss.app/embed/v1/carousel/2ytTHH88xrAVfOeI" frameborder="0"></iframe>
+        <iframe class="responsive-iframe" src="https://rss.app/embed/v1/carousel/2ytTHH88xrAVfOeI" frameborder="0"></iframe>
         </div>
       </div>
     </section>
