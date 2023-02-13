8e40246967232997b9dd83edc78b6e47672c4bba
Danielle Hoopes <daniellehoopes1@gmail.com>
2023-02-13 12:08:42 -0700

    Contact.js layout update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,6 +5,19 @@ const Contact = () => {
   return (
 <section id="contact" className="bg-secondary">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+    
+       <form action="/action_page.php"
+          netlify
+          name="contact"
+          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+            Hire Me
+          </h2>
+          <p className="leading-relaxed mb-5">
+           I am always happy to hear about projects your working on
+           and how I can help. 
+          </p>
+        </form>
         <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
           <iframe
             width="100%"
@@ -37,18 +50,6 @@ const Contact = () => {
             </div>
           </div>
         </div>
-       <form action="/action_page.php"
-          netlify
-          name="contact"
-          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-            Hire Me
-          </h2>
-          <p className="leading-relaxed mb-5">
-           I am always happy to hear about projects your working on
-           and how I can help. 
-          </p>
-        </form>
       </div>
     </section>
   );
