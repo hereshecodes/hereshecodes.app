09dc3bfa0e786a9d836250082d9be2b022b5df05
Danielle Hoopes <danielle.hoopes@snhu.edu>
2023-02-14 14:24:23 -0700

    News add

--- src/components/News.js
+++ src/components/News.js
@@ -2,9 +2,8 @@ import React from "react";
 
 const News = () => {
   return (
-    <section className="bg-primary text-white px-5 py-32" id="blog">
+    <section className="bg-secondery text-white" id="news">
      <iframe width="900" height="1600" src="https://rss.app/embed/v1/wall/tm3LNMRkF4alxPxW" frameborder="0"></iframe>
-     
     </section>
   );
 };
