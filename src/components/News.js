09ddafa100316f7eee40fc967f641c85166f1ed3
Danielle Hoopes <danielle.hoopes@snhu.edu>
2023-02-14 14:29:36 -0700

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const News = () => {
   return (
-    <section className="lg:w-3/4 bg-secondery text-white px-5 py-32" id="news">
-     <iframe width="900" height="1600" src="https://rss.app/embed/v1/wall/tm3LNMRkF4alxPxW" frameborder="0"></iframe>
+    <section className="bg-secondery text-white px-5 py-32" id="news">
+     <iframe width="900" height="900" src="https://rss.app/embed/v1/wall/tm3LNMRkF4alxPxW" frameborder="0"></iframe>
      
     </section>
   );
