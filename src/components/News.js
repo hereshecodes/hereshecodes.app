6d9b41d4d8816b64702e09c788c57c4702ebea51
Danielle Hoopes <daniellehoopes1@gmail.com>
2023-02-14 15:07:54 -0700

    newswrapper

--- src/components/News.js
+++ src/components/News.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const News = () => {
   return (
-    <section className="bg-secondery text-white px-5 py-32" id="news">
-     <iframe width="900" height="900" src="https://rss.app/embed/v1/wall/tm3LNMRkF4alxPxW" frameborder="0"></iframe>
+    <section className="newsWrapper bg-secondery text-white px-5 py-32" id="news">
+     <iframe  height="900" src="https://rss.app/embed/v1/wall/tm3LNMRkF4alxPxW" frameborder="0"></iframe>
         </section>
   );
 };
