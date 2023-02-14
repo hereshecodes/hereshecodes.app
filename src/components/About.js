1119bfa941359d65fb62651dafb091d7dde843ed
Danielle Hoopes <danielle.hoopes@snhu.edu>
2023-02-14 13:30:30 -0700

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -16,7 +16,7 @@ const About = () => {
 
 <p className="pb-5 text-accent">If you are here for my freelance work where I 
 work with clients to update their storefronts, WordPress sites, custom coded apps, and 
-lots more, head over to Fiver 
+lots more, head over to Fiverr 
 </p>
       
         <a
