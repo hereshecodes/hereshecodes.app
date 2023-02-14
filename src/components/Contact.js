014fb6bddd2c8bca3ce802e31fa7fa111866aca5
Danielle Hoopes <danielle.hoopes@snhu.edu>
2023-02-14 13:52:18 -0700

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -20,7 +20,7 @@ const Contact = () => {
         </form>
         <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
           <iframe
-            width="100%"
+            width="50%"
             height="100%"
             title="map"
             className="absolute inset-0"
