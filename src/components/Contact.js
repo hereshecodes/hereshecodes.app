1b7760f60e4a7c1babad740cb82a58e3b1e96625
Danielle Hoopes <danielle.hoopes@snhu.edu>
2023-02-14 14:00:52 -0700

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,7 +9,7 @@ const Contact = () => {
        <form action="/action_page.php"
           netlify
           name="contact"
-          className="lg:w-1/2 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          className="lg:w-1/4 md:w-1/4 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Hire Me
           </h2>
@@ -18,7 +18,7 @@ const Contact = () => {
            and how I can help. 
           </p>
         </form>
-        <div className="lg:w-1/2 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+        <div className="lg:w-3/4 md:w-3/4 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
           <iframe
             width="50%"
             height="100%"
@@ -31,7 +31,7 @@ const Contact = () => {
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d773465.8297770084!2d-112.47952465529956!3d40.77629923160011!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87523d9488d131ed%3A0x5b53b7a0484d31ca!2sSalt%20Lake%20City%2C%20UT!5e0!3m2!1sen!2sus!4v1676249437097!5m2!1sen!2sus" 
           />
           <div className="bg-gray-900 relative flex flex-wrap py-6 pr-8 rounded shadow-md">
-            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+            <div className="lg:w-3/4 px-6 mt-4 lg:mt-0">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
