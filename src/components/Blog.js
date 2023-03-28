b3225a0a36c820af91ed8c2860b8f6d087fa32c1
Danielle Hoopes <danielle.hoopes@snhu.edu>
2023-03-28 17:22:59 -0600

    Update Blog.js

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -7,23 +7,21 @@ const Blog = () => {
         Building a Conversational AI App with .NET, React, and ChatGPT
       </h1>
       <p>
-      Intro:
-
-Conversational AI has been a hot topic in the tech industry for several years now. With the increasing demand for chatbots and virtual assistants, building a conversational AI app has become a must for businesses of all sizes. In this blog post, we'll explore how to build a conversational AI app using .NET, React, and ChatGPT.
-
-Section 1: Introduction to .NET and React
-
-.NET is a free, open-source development platform created by Microsoft. It is used to build a variety of applications, including web, mobile, and desktop apps. React is a JavaScript library for building user interfaces, developed by Facebook. React is widely used in modern web development because of its simplicity and scalability.
-
-Section 2: Introduction to ChatGPT
-
-ChatGPT is a large language model developed by OpenAI. It is based on the GPT-3 architecture and is designed to generate human-like responses to natural language inputs. ChatGPT is a powerful tool for building conversational AI apps because it can understand natural language inputs and generate responses that sound like they were written by a human.
-
-Section 3: Building a Conversational AI App with .NET, React, and ChatGPT
-
-To build a conversational AI app with .NET, React, and ChatGPT, you'll need to follow these steps:
+        Conversational AI has been a hot topic in the tech industry for several years now. With the increasing demand for chatbots and virtual assistants, building a conversational AI app has become a must for businesses of all sizes. In this blog post, we'll explore how to build a conversational AI app using .NET, React, and ChatGPT.
       </p>
-      <h2>Step 1: Create a new .NET project</h2>
+      <h2>Section 1: Introduction to .NET and React</h2>
+      <p>
+        .NET is a free, open-source development platform created by Microsoft. It is used to build a variety of applications, including web, mobile, and desktop apps. React is a JavaScript library for building user interfaces, developed by Facebook. React is widely used in modern web development because of its simplicity and scalability.
+      </p>
+      <h2>Section 2: Introduction to ChatGPT</h2>
+      <p>
+        ChatGPT is a large language model developed by OpenAI. It is based on the GPT-3 architecture and is designed to generate human-like responses to natural language inputs. ChatGPT is a powerful tool for building conversational AI apps because it can understand natural language inputs and generate responses that sound like they were written by a human.
+      </p>
+      <h2>Section 3: Building a Conversational AI App with .NET, React, and ChatGPT</h2>
+      <p>
+        To build a conversational AI app with .NET, React, and ChatGPT, you'll need to follow these steps:
+      </p>
+      <h3>Step 1: Create a new .NET project</h3>
       <pre>
         <code>
           dotnet new webapi -n ChatGPTApp
@@ -31,7 +29,7 @@ To build a conversational AI app with .NET, React, and ChatGPT, you'll need to f
           dotnet add package Microsoft.AspNetCore.Cors
         </code>
       </pre>
-      <h2>Step 2: Create a new React project</h2>
+      <h3>Step 2: Create a new React project</h3>
       <pre>
         <code>
           npx create-react-app chat-gpt-app
@@ -39,28 +37,12 @@ To build a conversational AI app with .NET, React, and ChatGPT, you'll need to f
           npm install axios
         </code>
       </pre>
-      <h2>Step 3: Use the ChatGPT API to send natural language inputs to the model and receive responses</h2>
+      <h3>Step 3: Use the ChatGPT API to send natural language inputs to the model and receive responses</h3>
       <pre>
         <code>
-          // Paste the C# code block for using the ChatGPT API here
+          {/* Paste the C# code block for using the ChatGPT API here */}
         </code>
       </pre>
-      <h2>Step 4: Use .NET to create a back-end API</h2>
-      <pre>
- 
-       
-      </pre>
-      <h2>Step 5: Deploy the app to a hosting service</h2>
-      To deploy the app to Azure, you can follow these steps:
-
-Create an Azure account and log in to the Azure portal.
-Create a new App Service and configure it with the necessary settings, such as the runtime stack, deployment source, and environment variables.
-Deploy the app to the App Service using your preferred deployment method, such as Git, FTP, or Azure CLI.
-
-
-    </div>
-
-  );
+      <h3>Step 4: Use .NET to create a back-end API</h3>
+      </div>);
 };
-
-export default Blog;
