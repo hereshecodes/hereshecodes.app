e40ea9560a448902e3559a80def7cb09f95ad676
Danielle Hoopes <danielle.hoopes@snhu.edu>
2023-03-28 17:33:55 -0600

    Update Blog.js

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -7,42 +7,63 @@ const Blog = () => {
         Building a Conversational AI App with .NET, React, and ChatGPT
       </h1>
       <p>
-        Conversational AI has been a hot topic in the tech industry for several years now. With the increasing demand for chatbots and virtual assistants, building a conversational AI app has become a must for businesses of all sizes. In this blog post, we'll explore how to build a conversational AI app using .NET, React, and ChatGPT.
+        Intro:
+        <br />
+        Conversational AI has been a hot topic in the tech industry for several
+        years now. With the increasing demand for chatbots and virtual
+        assistants, building a conversational AI app has become a must for
+        businesses of all sizes. In this blog post, we'll explore how to build a
+        conversational AI app using .NET, React, and ChatGPT.
       </p>
-      <h2>Section 1: Introduction to .NET and React</h2>
+      <h2 className="text-xl font-bold my-4">Introduction to .NET and React</h2>
       <p>
-        .NET is a free, open-source development platform created by Microsoft. It is used to build a variety of applications, including web, mobile, and desktop apps. React is a JavaScript library for building user interfaces, developed by Facebook. React is widely used in modern web development because of its simplicity and scalability.
+        .NET is a free, open-source development platform created by Microsoft.
+        It is used to build a variety of applications, including web, mobile,
+        and desktop apps. React is a JavaScript library for building user
+        interfaces, developed by Facebook. React is widely used in modern web
+        development because of its simplicity and scalability.
       </p>
-      <h2>Section 2: Introduction to ChatGPT</h2>
+      <h2 className="text-xl font-bold my-4">Introduction to ChatGPT</h2>
       <p>
-        ChatGPT is a large language model developed by OpenAI. It is based on the GPT-3 architecture and is designed to generate human-like responses to natural language inputs. ChatGPT is a powerful tool for building conversational AI apps because it can understand natural language inputs and generate responses that sound like they were written by a human.
+        ChatGPT is a large language model developed by OpenAI. It is based on
+        the GPT-3 architecture and is designed to generate human-like responses
+        to natural language inputs. ChatGPT is a powerful tool for building
+        conversational AI apps because it can understand natural language inputs
+        and generate responses that sound like they were written by a human.
       </p>
-      <h2>Section 3: Building a Conversational AI App with .NET, React, and ChatGPT</h2>
-      <p>
-        To build a conversational AI app with .NET, React, and ChatGPT, you'll need to follow these steps:
-      </p>
-      <h3>Step 1: Create a new .NET project</h3>
-      <pre>
-        <code>
-          dotnet new webapi -n ChatGPTApp
-          cd ChatGPTApp
-          dotnet add package Microsoft.AspNetCore.Cors
-        </code>
+      <h2 className="text-xl font-bold my-4">
+        Building a Conversational AI App with .NET, React, and ChatGPT
+      </h2>
+      <li>Create a new React project</li>
+<pre className="bg-gray-100 p-4 rounded-lg my-4">
+  <code className="text-sm font-mono">
+    npx create-react-app chat-gpt-app
+  </code>
+</pre>
+<p className="mb-4">
+  This will create a new React project called "chat-gpt-app" in your current directory. Once the project is created, navigate to the project directory using:
+</p>
+<pre className="bg-gray-100 p-4 rounded-lg my-4">
+  <code className="text-sm font-mono">
+    cd chat-gpt-app
+  </code>
+</pre>
+<p className="mb-4">
+  Next, you'll need to install the "axios" package, which will be used to make HTTP requests to your .NET back-end API:
+</p>
+<pre className="bg-gray-100 p-4 rounded-lg my-4">
+  <code className="text-sm font-mono">
+    npm install axios
+  </code>
+</pre>
+<h2>Use the ChatGPT API to send natural language inputs to the model and receive responses</h2>
+      <pre className="bg-gray-100 p-4 rounded-lg my-4">
+        <code className="block">{`// Paste the JavaScript code block for using the ChatGPT API here`}</code>
+      </pre> 
+      <h2>Step 4: Use .NET to create a back-end API</h2>
+      <pre className="bg-gray-100 p-4 rounded-lg my-4">
+        <code className="block">{`// Paste the C# code block for creating a back-end API here`}</code>
       </pre>
-      <h3>Step 2: Create a new React project</h3>
-      <pre>
-        <code>
-          npx create-react-app chat-gpt-app
-          cd chat-gpt-app
-          npm install axios
-        </code>
-      </pre>
-      <h3>Step 3: Use the ChatGPT API to send natural language inputs to the model and receive responses</h3>
-      <pre>
-        <code>
-          {/* Paste the C# code block for using the ChatGPT API here */}
-        </code>
-      </pre>
-      <h3>Step 4: Use .NET to create a back-end API</h3>
-      </div>);
+</div>
+);
 };
