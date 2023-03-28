98432c2e6e50ba7b6a53206af39abaf4dc5074be
Danielle Hoopes <daniellehoopes1@gmail.com>
2023-03-28 16:55:52 -0600

    New blog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,14 +2,109 @@ import React from "react";
 
 const Blog = () => {
   return (
-    <section className="bg-primary text-white px-5 py-32" id="blog">
-      <div className="container mx-auto  items-center justify-center md:justify-between">
-        <div className="stack-carousel scrollbar-hide overflow-auto">
-        <iframe width="500" height="400"  src="https://rss.app/embed/v1/carousel/2ytTHH88xrAVfOeI" frameborder="0"></iframe>
-        </div>
-      </div>
-    </section>
+    <div className="container mx-auto my-8">
+      <h1 className="text-3xl font-bold mb-4">
+        Building a Conversational AI App with .NET, React, and ChatGPT
+      </h1>
+      <p>
+      Intro:
+
+Conversational AI has been a hot topic in the tech industry for several years now. With the increasing demand for chatbots and virtual assistants, building a conversational AI app has become a must for businesses of all sizes. In this blog post, we'll explore how to build a conversational AI app using .NET, React, and ChatGPT.
+
+Section 1: Introduction to .NET and React
+
+.NET is a free, open-source development platform created by Microsoft. It is used to build a variety of applications, including web, mobile, and desktop apps. React is a JavaScript library for building user interfaces, developed by Facebook. React is widely used in modern web development because of its simplicity and scalability.
+
+Section 2: Introduction to ChatGPT
+
+ChatGPT is a large language model developed by OpenAI. It is based on the GPT-3 architecture and is designed to generate human-like responses to natural language inputs. ChatGPT is a powerful tool for building conversational AI apps because it can understand natural language inputs and generate responses that sound like they were written by a human.
+
+Section 3: Building a Conversational AI App with .NET, React, and ChatGPT
+
+To build a conversational AI app with .NET, React, and ChatGPT, you'll need to follow these steps:
+      </p>
+      <h2>Step 1: Create a new .NET project</h2>
+      <pre>
+        <code>
+          dotnet new webapi -n ChatGPTApp
+          cd ChatGPTApp
+          dotnet add package Microsoft.AspNetCore.Cors
+        </code>
+      </pre>
+      <h2>Step 2: Create a new React project</h2>
+      <pre>
+        <code>
+          npx create-react-app chat-gpt-app
+          cd chat-gpt-app
+          npm install axios
+        </code>
+      </pre>
+      <h2>Step 3: Use the ChatGPT API to send natural language inputs to the model and receive responses</h2>
+      <pre>
+        <code>
+          // Paste the C# code block for using the ChatGPT API here
+        </code>
+      </pre>
+      <h2>Step 4: Use .NET to create a back-end API</h2>
+      <pre>
+        <code>
+        using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ChatGPTApp.Services
+{
+    public class ChatGPTService
+    {
+        private readonly string _endpointUrl = "https://api.openai.com/v1/engines/davinci-codex/completions";
+
+        public async Task<string> GenerateResponseAsync(string prompt)
+        {
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", "YOUR_API_KEY_HERE");
+
+            var request = new
+            {
+                prompt = prompt,
+                max_tokens = 150,
+                n = 1,
+                stop = "\n",
+                temperature = 0.7
+            };
+
+            var jsonRequest = JsonConvert.SerializeObject(request);
+
+            var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(_endpointUrl, content);
+
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            dynamic responseObject = JsonConvert.DeserializeObject(jsonResponse);
+            var generatedText = responseObject.choices[0].text;
+
+            return generatedText;
+        }
+    }
+}
+        </code>
+      </pre>
+      <h2>Step 5: Deploy the app to a hosting service</h2>
+      To deploy the app to Azure, you can follow these steps:
+
+Create an Azure account and log in to the Azure portal.
+Create a new App Service and configure it with the necessary settings, such as the runtime stack, deployment source, and environment variables.
+Deploy the app to the App Service using your preferred deployment method, such as Git, FTP, or Azure CLI.
+
+
+
+    </div>
+  
+
+    </div>
   );
-};
+}
 
 export default Blog;
