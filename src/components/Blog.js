c96198d1ee49c54d3d4148f702c3d5094f3e8f86
Danielle Hoopes <danielle.hoopes@snhu.edu>
2023-03-28 18:08:56 -0600

    blog updates

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Blog = () => {
   return (
-    <div className="container mx-auto my-8">
+    <div className="blog-container mx-auto my-8">
       <div class="main-post">
       <h1 className="text-3xl font-bold mb-4">
         Building a Conversational AI App with .NET, React, and ChatGPT
