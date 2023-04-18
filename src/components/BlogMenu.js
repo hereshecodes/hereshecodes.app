68a0f7c4005430aa74b6733a443d7a2b197d5ab8
Danielle Hoopes <danielle.hoopes@snhu.edu>
2023-04-18 10:06:44 -0600

    new images for blog

--- src/components/BlogMenu.js
+++ src/components/BlogMenu.js
@@ -6,7 +6,7 @@ import React from "react";
  
     <div className="grid grid-cols-3 gap-4">
       <div className="blog-card">
-        <div className="blog-card-image" style={{backgroundImage: 'url(https://source.unsplash.com/random/400x400)'}}></div>
+        <div className="blog-card-image" style={{backgroundImage: 'url(/public/src/assets/dotnet.jpg) '}}></div>
         <div className="blog-card-content">
           <h2 className="text-xl font-bold mb-2">Building a Conversational AI App with .NET, React, and ChatGPT</h2>
           <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
