b2eedd3dc176c525e268ab41f0b1ac6a85ae7a2a
Danielle Hoopes <danielle.hoopes@snhu.edu>
2023-04-18 10:27:04 -0600

    Update BlogMenu.js

--- src/components/BlogMenu.js
+++ src/components/BlogMenu.js
@@ -6,11 +6,11 @@ import React from "react";
  
     <div className="grid grid-cols-3 gap-4">
       <div className="blog-card">
-        <div className="blog-card-image" style={{backgroundImage: 'url(/src/assets/dotnet.jpg) '}}></div>
+        <div className="blog-card-image" style={{backgroundImage: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Microsoft_.NET_logo.svg/225px-Microsoft_.NET_logo.svg.png) '}}></div>
         <div className="blog-card-content">
           <h2 className="text-xl font-bold mb-2">Building a Conversational AI App with .NET, React, and ChatGPT</h2>
           <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-          <a href="/src/components/Blog.js" className="btn btn-primary mt-4">Read More</a>
+          <a href="/blog" className="btn btn-primary mt-4">Read More</a>
         </div>
       </div>
      
