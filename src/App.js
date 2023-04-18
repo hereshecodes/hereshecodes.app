68a847be503e6a1c73c37dd352d019b448c780cd
Danielle Hoopes <danielle.hoopes@snhu.edu>
2023-04-18 10:46:44 -0600

    remove blog for now until I fix it

--- src/App.js
+++ src/App.js
@@ -23,7 +23,6 @@ export default function App() {
       </BrowserRouter>
       <Hero/>
       <About/>
-      <BlogMenu/>
       <News/>
       <Projects/>
       <Contact/>
--- src/components/BlogMenu.js
+++ src/components/BlogMenu.js
@@ -9,7 +9,10 @@ import React from "react";
         <div className="blog-card-image" style={{backgroundImage: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Microsoft_.NET_logo.svg/150px-Microsoft_.NET_logo.svg.png) '}}></div>
         <div className="blog-card-content">
           <h2 className="text-xl font-bold mb-2">Building a Conversational AI App with .NET, React, and ChatGPT</h2>
-             
+          <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+          <Link to="/blog" className="btn btn-primary mt-4">
+              Read More
+            </Link>      
               </div>
       </div>
 
