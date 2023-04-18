5e8efb2a2e7d09eea7d3bbcbf3427440bef19169
Danielle Hoopes <danielle.hoopes@snhu.edu>
2023-04-18 09:59:14 -0600

    Update BlogMenu.js

--- src/components/BlogMenu.js
+++ src/components/BlogMenu.js
@@ -1,39 +1,37 @@
 import React from "react";
-import BlogMenu from "./BlogMenu";
 
-const BlogMenu = () => {
-  return (
-    <div className="blog-container mx-auto my-8">
-   
-      <div className="grid grid-cols-3 gap-4">
-        <div className="blog-card">
-          <div className="blog-card-image" style={{backgroundImage: 'url(https://source.unsplash.com/random/400x400)'}}></div>
-          <div className="blog-card-content">
-            <h2 className="text-xl font-bold mb-2">Building a Conversational AI App with .NET, React, and ChatGPT</h2>
-            <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-            <a href="/src/components/Blog.js" className="btn btn-primary mt-4">Read More</a>
-          </div>
+ const BlogMenu = () => {
+ return (
+  <div className="blog-container mx-auto my-8">
+ 
+    <div className="grid grid-cols-3 gap-4">
+      <div className="blog-card">
+        <div className="blog-card-image" style={{backgroundImage: 'url(https://source.unsplash.com/random/400x400)'}}></div>
+        <div className="blog-card-content">
+          <h2 className="text-xl font-bold mb-2">Building a Conversational AI App with .NET, React, and ChatGPT</h2>
+          <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+          <a href="/src/components/Blog.js" className="btn btn-primary mt-4">Read More</a>
         </div>
-        <div className="blog-card">
-          <div className="blog-card-image" style={{backgroundImage: 'url(https://source.unsplash.com/random/400x400)'}}></div>
-          <div className="blog-card-content">
-            <h2 className="text-xl font-bold mb-2">Blog Post Title</h2>
-            <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-            <a href="#" className="btn btn-primary mt-4">Read More</a>
-          </div>
-        </div>
-        <div className="blog-card">
-          <div className="blog-card-image" style={{backgroundImage: 'url(https://source.unsplash.com/random/400x400)'}}></div>
-          <div className="blog-card-content">
-            <h2 className="text-xl font-bold mb-2">Blog Post Title</h2>
-            <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-            <a href="#" className="btn btn-primary mt-4">Read More</a>
-          </div>
-        </div>
-        {/* Add more blog cards here */}
       </div>
+      <div className="blog-card">
+        <div className="blog-card-image" style={{backgroundImage: 'url(https://source.unsplash.com/random/400x400)'}}></div>
+        <div className="blog-card-content">
+          <h2 className="text-xl font-bold mb-2">Blog Post Title</h2>
+          <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+          <a href="#" className="btn btn-primary mt-4">Read More</a>
+        </div>
+      </div>
+      <div className="blog-card">
+        <div className="blog-card-image" style={{backgroundImage: 'url(https://source.unsplash.com/random/400x400)'}}></div>
+        <div className="blog-card-content">
+          <h2 className="text-xl font-bold mb-2">Blog Post Title</h2>
+          <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+          <a href="#" className="btn btn-primary mt-4">Read More</a>
+        </div>
+      </div>
+      {/* Add more blog cards here */}
     </div>
-  );
+  </div>
+);
 };
 
-export default BlogMenu;
