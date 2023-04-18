e5f626632a0a950e3a213531e45da87bf7d69bc0
Danielle Hoopes <danielle.hoopes@snhu.edu>
2023-04-18 10:48:56 -0600

    Update BlogMenu.js

--- src/components/BlogMenu.js
+++ src/components/BlogMenu.js
@@ -10,10 +10,8 @@ import React from "react";
         <div className="blog-card-content">
           <h2 className="text-xl font-bold mb-2">Building a Conversational AI App with .NET, React, and ChatGPT</h2>
           <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-          <Link to="/blog" className="btn btn-primary mt-4">
-              Read More
-            </Link>      
-              </div>
+          <a href="/src/components/Blog.js" className="btn btn-primary mt-4">Read More</a>
+        </div>
       </div>
 
     </div>
