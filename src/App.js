a4de7ac5f351a7e2f90ae8b8fcf05cfe2175a063
Danielle Hoopes <danielle.hoopes@snhu.edu>
2023-04-18 10:36:12 -0600

    f

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router";
 import About from "./components/About";
 import Blog from "./components/Blog";
 import BlogMenu from "./components/BlogMenu";
