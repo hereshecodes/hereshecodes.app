ff4834bca9c5c0083561cf797fca5c68c6a96fab
Danielle Hoopes <danielle.hoopes@snhu.edu>
2023-08-03 11:42:38 -0500

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -57,7 +57,7 @@ const Hero = () => {
               target="_blank" className="pr-4 inline-block text-accent hover:text-white"
             >
               {" "}
-              <AiOutlineGithub size={40} />{" "}
+              <AiOutlineBehanceSquare size={40} />{" "}
             </a>
           </div>
 
