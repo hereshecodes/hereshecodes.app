8fc5733cc186b8ab8d3f3712c842397cb5239da2
Danielle Hoopes <danielle.hoopes@snhu.edu>
2023-08-03 11:35:58 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Contact from "./components/Contact";
 import News from "./components/News";
-import Projects from "./components/Projects";
+
 import Footer from  "./components/Footer";
 
 export default function App() {
@@ -22,7 +22,7 @@ export default function App() {
       </BrowserRouter>
       <Hero/>
       <About/>
-      <Projects/>
+
       <News/>
       <Contact/>
       <Footer/>
