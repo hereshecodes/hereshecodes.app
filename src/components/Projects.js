d2a3175edad5248c70adfc38781a7254a6d85135
Danielle Hoopes <daniellehoopes1@gmail.com>
2024-02-03 10:46:34 -0600

    Update to live projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,16 +7,16 @@ const Projects = () => {
         <div className="about-info">
         
        
-          {/* Live Templates
+        
           <div>
             <h2 className="text-lg font-semibold mb-2">Live Templates</h2>
             <div className="grid grid-cols-2 gap-4">
-              {/* Template 1 
+     
               <div className="bg-white p-4">
-                <h3 className="text-xl font-semibold">Template 1</h3>
-                <p>Description of Template 1</p>
+                <h3 className="text-xl font-semibold">SB Lash and Wax - Beauty Services</h3>
+                <p>Aesthetics Website</p>
                 <a
-                  href="link-to-template-1"
+                  href="https://sb-lash-and-wax-fort-smith-git-master-daniellehoopes.vercel.app/services.html"
                   className="text-blue-500 underline"
                   target="_blank"
                   rel="noopener noreferrer"
@@ -24,55 +24,12 @@ const Projects = () => {
                   View Template
                 </a>
               </div>
-              {/* Template 2 *
-              <div className="bg-white p-4">
-                <h3 className="text-xl font-semibold">Template 2</h3>
-                <p>Description of Template 2</p>
-                <a
-                  href="link-to-template-2"
-                  className="text-blue-500 underline"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  View Template
-                </a>
-              </div>
-            </div>
-  </div> */}
+           
           {/* Projects */}
-          <div>
-            <h2 className="text-lg font-semibold mb-2">Templates</h2>
-            <div className="grid grid-cols-2 gap-4">
-              {/* Project 1 */}
-              <div className="bg-white p-4">
-                <h3 className="text-xl font-semibold">Project 1</h3>
-                <p>Description of Project 1</p>
-                <a
-                  href="https://sidebar-bootstrap-template-44p3bhbca-daniellehoopes.vercel.app/index.html"
-                  className="text-blue-500 underline"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Bootstrap Simple Template
-                </a>
-              </div>
-              {/* Project 2 */}
-              <div className="bg-white p-4">
-                <h3 className="text-xl font-semibold">Project 2</h3>
-                <p>Description of Project 2</p>
-                <a
-                  href="link-to-project-2"
-                  className="text-blue-500 underline"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  View Project
-                </a>
-              </div>
-            </div>
-          </div>
+         
         </div>
-       
+       </div>
+      </div>
       </div>
     </section>
   );
