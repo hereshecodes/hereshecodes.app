fd134fcf9473d04f9a7a29e90cababa9b9b853c3
Danielle Hoopes <daniellehoopes1@gmail.com>
2024-07-14 12:02:20 -0600

    font change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="bg-gray-800 py-4">
       <div className="container mx-auto text-center text-gray-400">
-        &copy; {new Date().getFullYear()} My Portfolio. All rights reserved.
+        &copy; {new Date().getFullYear()} Made with React.
       </div>
     </footer>
   );
