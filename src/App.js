d24f600015eef359dff9cf19061cb788fb4e6d53
Danielle Hoopes <daniellehoopes1@gmail.com>
2024-07-14 12:08:40 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import React from "react";
+import React, { useEffect } from 'react';
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Navbar from "./components/Navbar";
