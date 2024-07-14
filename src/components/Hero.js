cdd7c486d75d631c0c8a6c4d1f7b69bbe0d95b9d
Danielle Hoopes <danielle.hoopes@snhu.edu>
2024-07-14 11:24:38 -0600

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,7 +10,7 @@ function Hero() {
       <div className="container mx-auto flex flex-col md:flex-row items-center">
         <div className="text-center md:text-left md:w-1/2">
           <p className="text-xl text-gray-200 mt-4">I am a fullstack developer specializing in a variety of web technologies and .NET.</p>
-          <a href="#projects" className="mt-8 px-6 py-2 bg-primary text-white rounded hover:bg-hover-primary inline-block">View My Work</a>
+          {/* <a href="#projects" className="mt-8 px-6 py-2 bg-primary text-white rounded hover:bg-hover-primary inline-block">View My Work</a> */}
         </div>
         <div className="md:w-1/2">
           <img src={heroImage} alt="Hero" className="w-full h-auto object-cover"/>
