c5d52f0b89f7eadc31d763d8a1538a7cc4c71633
Danielle Hoopes <danielle.hoopes@snhu.edu>
2024-07-14 11:03:14 -0600

    pfp

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import backgroundImage from '../assets/BLOG.png'; 
-
+import backgroundImage from '../assets/BLOG.png'; // Update the path accordingly
+import heroImage from '../assets/pfp.png'; // Update the path accordingly
 
 function Hero() {
   return (
     <section 
       className="hero-section bg-cover bg-center h-screen flex items-center justify-center" 
       style={{ backgroundImage: `url(${backgroundImage})` }}>
-      <div className="text-center">
-        <h1 className="text-5xl font-bold text-white">Welcome to My Portfolio</h1>
-        <p className="text-xl text-gray-200 mt-4">I am a freelance developer specializing in React apps.</p>
-        <a href="#projects" className="mt-8 px-6 py-2 bg-primary text-white rounded hover:bg-hover-primary">View My Work</a>
+      <div className="container mx-auto flex flex-col md:flex-row items-center">
+        <div className="text-center md:text-left md:w-1/2">
+          <h1 className="text-5xl font-bold text-white">Welcome to My Portfolio</h1>
+          <p className="text-xl text-gray-200 mt-4">I am a freelance developer specializing in React apps.</p>
+          <a href="#projects" className="mt-8 px-6 py-2 bg-primary text-white rounded hover:bg-hover-primary inline-block">View My Work</a>
+        </div>
+        <div className="md:w-1/2">
+          <img src={heroImage} alt="Hero" className="w-full h-auto object-cover"/>
+        </div>
       </div>
     </section>
   );
